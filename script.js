48611b5a950842b31fb0edd3fd4f044e6e582fa8
splean101 <101splean@gmail.com>
2022-12-30 14:04:52 +0200

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+//1.
+
+// let userAge = Number(prompt('Please enter your age!', 'your age'));
+// if(userAge <= 17){
+//   confirm('Do you study at school?')
+// } else if (userAge > 17 && userAge < 25){
+//   confirm('Do you study at university?')
+// } else if (userAge >= 25 && userAge < 60){
+//   confirm('Do you have a job?')
+// } else {
+//   confirm('What are you doing?')
+// }
+
+//2.
+let display = prompt(
+  'Enter the display value',
+  'block, flex, grid, inline or none'
+);
+
+//3.
+
+//4.
