8bd3980f37a06a2d34a5cd5582070e47bfd0eeaf
splean101 <101splean@gmail.com>
2023-01-04 20:00:15 +0200

    did

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 //1.
 
-// let userAge = Number(prompt('Please enter your age!', 'your age'));
-// if(userAge <= 17){
-//   confirm('Do you study at school?')
-// } else if (userAge > 17 && userAge < 25){
-//   confirm('Do you study at university?')
-// } else if (userAge >= 25 && userAge < 60){
-//   confirm('Do you have a job?')
-// } else {
-//   confirm('What are you doing?')
-// }
+let userAge = Number(prompt('Please enter your age!', 'your age'));
+if(userAge <= 17){
+  confirm('Do you study at school?')
+} else if (userAge > 17 && userAge < 25){
+  confirm('Do you study at university?')
+} else if (userAge >= 25 && userAge < 60){
+  confirm('Do you have a job?')
+} else {
+  confirm('What are you doing?')
+}
 
 //2.
 
-// let display = prompt(
-//   'Enter the display value',
-//   'block, flex, grid, inline or none'
-// );
+let display = prompt(
+  'Enter the display value',
+  'block, flex, grid, inline or none'
+);
 
-// switch (display) {
-//   case 'block':
-//     alert('block');
-//     break;
-//   case 'flex':
-//     alert('flex');
-//     break;
-//   case 'grid':
-//     alert('grid');
-//     break;
-//   case 'inline':
-//     alert('inline');
-//     break;
-//   case 'none':
-//     alert('none');
-//     break;
-//   default:
-//     alert("Set 'display: inline-block'");
-// }
+switch (display) {
+  case 'block':
+    alert('block');
+    break;
+  case 'flex':
+    alert('flex');
+    break;
+  case 'grid':
+    alert('grid');
+    break;
+  case 'inline':
+    alert('inline');
+    break;
+  case 'none':
+    alert('none');
+    break;
+  default:
+    alert("Set 'display: inline-block'");
+}
 
 //3.
 let positiveNumber = Number(prompt('Please enter positive number'));
@@ -52,11 +52,9 @@ while (true) {
   );
 }
 
-//
-
 //4.
-// for(let i = 2; i <= 50; i++){
-//   if(i%2 ===0){
-//     console.log(i);
-//   }
-// }
+for(let i = 2; i <= 50; i++){
+  if(i%2 ===0){
+    console.log(i);
+  }
+}
